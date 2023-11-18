47e3a2ad9dfd72aeac8aff250c2715bc22782e4d
DanielaLosada <danielai.losadar@uqvirtual.edu.co>
2023-11-18 14:49:27 -0500

    recuperacion

--- src/controllers/ModelFactoryController.java
+++ src/controllers/ModelFactoryController.java
@@ -82,11 +82,7 @@ public class ModelFactoryController {
     	admin.setUsuario("pepito12");
     	admin.setContrasenia("12345");
 
-<<<<<<< Updated upstream
     }
-=======
-    }
->>>>>>> Stashed changes
 	
 	public Restaurante getRed() {
 		return restaurante;
